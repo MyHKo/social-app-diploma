dee205e111beb410065c814e034fb217eeefa40e
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-18 09:46:55 +0100

    Backend: add post controller

---
+++ app/backend/src/main/java/org/socialapp/controller/PostController.java
@@ -0,0 +1,33 @@
+package org.socialapp.controller;
+
+import org.socialapp.Service.PostService;
+import org.socialapp.Service.UserService;
+import org.socialapp.model.Entity.PostEntity;
+import org.socialapp.model.Entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+    private UserService userService;
+
+    @GetMapping
+    public String allPosts() {
+        return new String("You cannot check all of the posts");
+    }
+
+    @GetMapping("/posts/{id}")
+    public List<PostEntity> getPostsByUser_id(@PathVariable String id) {
+        UserEntity user = userService.getUserByEmail(id);
+        return postService.getPostsByUser(user);
+    }
+}
