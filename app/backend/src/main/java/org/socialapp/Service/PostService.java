a1e5150cdb7b86ed55e816a6f60ad9d2ec1af340
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-18 09:31:19 +0100

    Backend: add post service class

---
+++ app/backend/src/main/java/org/socialapp/Service/PostService.java
@@ -0,0 +1,28 @@
+package org.socialapp.Service;
+
+import org.socialapp.model.Entity.PostEntity;
+import org.socialapp.model.Entity.UserEntity;
+import org.socialapp.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<PostEntity> getPostsByUser(UserEntity user) {
+        return postRepository.findAllByUser_id(user);
+    }
+
+    public PostEntity createPost(PostEntity post) {
+        return postRepository.save(post);
+    }
+
+    public void deletePost(PostEntity post) {
+        postRepository.delete(post);
+    }
+}
--- app/backend/src/main/java/org/socialapp/repository/PostRepository.java
+++ app/backend/src/main/java/org/socialapp/repository/PostRepository.java
@@ -5,9 +5,11 @@ import org.socialapp.model.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
-    PostEntity findByUser_id(UserEntity user);
+    List<PostEntity> findAllByUser_id(UserEntity user);
 
 }
