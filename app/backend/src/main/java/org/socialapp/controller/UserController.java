781742bbc9b8e8ec8adfa7187d01b8be2e968c95
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-20 16:58:21 +0100

    Backend: add temporary controller

--- app/backend/src/main/java/org/socialapp/controller/UserController.java
+++ app/backend/src/main/java/org/socialapp/controller/UserController.java
@@ -1,9 +1,12 @@
 package org.socialapp.controller;
 
+import org.socialapp.Service.SubscriptionService;
 import org.socialapp.Service.UserService;
+import org.socialapp.model.Entity.SubscriptionEntity;
 import org.socialapp.model.Entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +19,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private SubscriptionService subscriptionService;
+
     @GetMapping
     public List<UserEntity> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable("id") String id) {
+        return userService.getUserByEmail(id);
+    }
+
+    @GetMapping("/{id}/subscriptions")
+    public List<SubscriptionEntity> getUserSubscriptions(@PathVariable("id") String id) {
+        UserEntity user = userService.getUserByEmail(id);
+        return subscriptionService.getAllSubscriptions(user);
     }
 }
