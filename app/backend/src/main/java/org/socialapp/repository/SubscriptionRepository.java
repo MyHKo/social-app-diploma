57dd26bb50856760dfadee9afc240d29056a7db8
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-20 16:30:19 +0100

    Backend: add subscription repository

--- app/backend/src/main/java/org/socialapp/repository/SubscriptionRepository.java
+++ app/backend/src/main/java/org/socialapp/repository/SubscriptionRepository.java
@@ -1,4 +1,17 @@
 package org.socialapp.repository;
 
-public interface SubscriptionRepository {
+import org.socialapp.model.Entity.SubscriptionEntity;
+import org.socialapp.model.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Long> {
+
+    List<SubscriptionEntity> findBySubscriber(UserEntity subscriber);
+
+    List<SubscriptionEntity> findBySubscribee(UserEntity subscribee);
+
 }
