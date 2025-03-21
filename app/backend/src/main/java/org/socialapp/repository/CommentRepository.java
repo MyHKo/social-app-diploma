58900ee8ec3c94d5df930d3e3972b095e8ce03cb
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-21 18:21:46 +0100

    Backend: add Comment Repository

---
+++ app/backend/src/main/java/org/socialapp/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package org.socialapp.repository;
+
+import org.socialapp.model.Entity.CommentEntity;
+import org.socialapp.model.Entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+
+    List<CommentEntity> findAllByPost(PostEntity post);
+
+}
+
