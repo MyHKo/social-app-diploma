8941ba7aee297a0cc8df897feb8e27a6b728786d
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-03-31 07:58:22 +0200

    Paper: add table of Contents page

--- app/backend/src/main/java/org/socialapp/model/Entity/UserEntity.java
+++ app/backend/src/main/java/org/socialapp/model/Entity/UserEntity.java
@@ -2,7 +2,6 @@ package org.socialapp.model.Entity;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Pattern;
