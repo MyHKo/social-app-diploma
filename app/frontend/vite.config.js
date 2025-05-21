539adb27723e3db88c245fa648fa42b6fee2e728
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-21 21:15:28 +0200

    Frontend: vite config

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,7 +1,45 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHash } from 'crypto'
+import path from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@routing': path.resolve(__dirname, 'src/routing'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+    },
+  },
+  css: {
+    modules: {
+      generateScopedName: (className, fileName, css) => {
+        const componentName = fileName
+            .split('/')
+            .pop()
+            .replace(/\.module\.(css|scss)$/, '')
+
+        const hash = createHash('sha1').update(css).digest('hex').substring(0, 5)
+
+        return `${componentName}_${className}_${hash}`
+      },
+    },
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 })
