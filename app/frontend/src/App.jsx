1c2e7aac401d73d31e2af0ea7e7253ac403b2d1d
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-22 16:46:31 +0200

    Frotnend: add styles to the main container

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from '@pages/Home/Home.jsx'
+import Header from'@components/Header/Header.jsx'
 import './App.scss'
 
 function App() {
 
   return (
-    <>
+    <div className='main-container'>
+      <Header />
       <Home />
-    </>
+    </div>
   )
 }
 
