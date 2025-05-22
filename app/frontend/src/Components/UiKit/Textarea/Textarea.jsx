9f5a79ffcc17e93ea21660ac3a12da0534c1ca2d
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-22 18:24:08 +0200

    Frontend: add text area component

--- app/frontend/src/Components/UiKit/Textarea/Textarea.jsx
+++ app/frontend/src/Components/UiKit/Textarea/Textarea.jsx
@@ -1,7 +1,13 @@
-import styles from '/textarea.module.scss'
+import styles from '/src/Components/UiKit/Textarea/textarea.module.scss'
+import PropTypes from "prop-types";
 
- function Textarea({ value }) {
-    return <textarea className={``} {...props} />;
+ function Textarea({ value, placeholder }) {
+    return <textarea className={styles.textarea} value={value} placeholder={placeholder} />;
 }
 
 export default Textarea
+
+Textarea.propTypes = {
+    value: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired
+}
