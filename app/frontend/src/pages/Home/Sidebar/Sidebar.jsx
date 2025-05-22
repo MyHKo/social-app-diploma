5e2c987eec04ec482c1d7ec864c4fd92de70430e
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-22 18:29:40 +0200

    Frontend: fix styles of sidebar

--- app/frontend/src/pages/Home/Sidebar/Sidebar.jsx
+++ app/frontend/src/pages/Home/Sidebar/Sidebar.jsx
@@ -4,15 +4,15 @@ import styles from  './sidebar.module.scss'
 function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <div className={styles.sidebarSection}>
-                <div className={styles.profileLink}>
+            <div className={styles.sidebar_section}>
+                <div className={styles.profile_link}>
                     <User className="icon" />
-                    <span className={styles.profileText}>Profile</span>
+                    <span className={styles.profile_text}>Profile</span>
                 </div>
             </div>
-            <div className={styles.sidebarSection}>
-                <span className={styles.sectionTitle}>Trending Topics</span>
-                <ul className={styles.trendingList}>
+            <div className={styles.sidebar_section}>
+                <span className={styles.section_title}>Trending Topics</span>
+                <ul className={styles.trending_list}>
                     <li>#Dogs</li>
                     <li>#Cats</li>
                     <li>#Computer Science</li>
