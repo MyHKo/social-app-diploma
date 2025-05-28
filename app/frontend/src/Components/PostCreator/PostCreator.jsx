e238c71ea9b768e72cb4b73daa3a5ebaa2d7ea03
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-28 10:51:33 +0200

    Frontend: implememnt post creator component

---
+++ app/frontend/src/Components/PostCreator/PostCreator.jsx
@@ -0,0 +1,14 @@
+import styles from './PostCreator.module.scss';
+import Textarea from '@components/UiKit/Textarea/Textarea.jsx'
+import Button from '@components/UiKit/Button/Button.jsx'
+
+function PostCreator() {
+    return (
+        <div className={styles.postCreator}>
+            <Textarea placeholder="What's on your mind?" value={""}/>
+            <Button text={"Post"}/>
+        </div>
+    )
+}
+
+export default PostCreator
