b56d28c08136e931b556c81fef5ed16e65300dfb
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-28 12:32:52 +0200

    Frontend: add Posts component to the pages and fix styles

--- app/frontend/src/pages/Home/Feed/Feed.jsx
+++ app/frontend/src/pages/Home/Feed/Feed.jsx
@@ -1,6 +1,6 @@
 import PostCreator from '@components/PostCreator/PostCreator.jsx'
-import { Heart, MessageCircle } from 'lucide-react'
 import styles from './feed.module.scss'
+import Post from '@components/Post/index.jsx'
 
 function Feed() {
     return (
@@ -8,19 +8,7 @@ function Feed() {
             <PostCreator />
 
             {[1, 2, 3].map((id) => (
-                <div className={styles.post} key={id}>
-                    <div className={styles.postHeader}>
-                        <span className={styles.username}>@user{id}</span>
-                        <span className={styles.timestamp}>2h ago</span>
-                    </div>
-                    <p className={styles.postContent}>
-                        This is a sample post content. Loving this new platform!
-                    </p>
-                    <div className={styles.postActions}>
-                        <Heart className={styles.icon} />
-                        <MessageCircle className={styles.icon} />
-                    </div>
-                </div>
+                <Post key={id} user={"@user"} text={"This is my first post. Loving this platform"} time={"2"} />
             ))}
         </section>
     );
--- app/frontend/src/pages/Profile/index.jsx
+++ app/frontend/src/pages/Profile/index.jsx
@@ -1,35 +1,40 @@
 import styles from './profile.module.scss'
+import Post from "@components/Post/index.jsx";
 
 function Profile() {
     return(
         <div className={styles.profile_container}>
-            <div className={styles.header}>
-                <div className={styles.avatar}></div>
-                <div>
-                    <h2 className={styles.username}>@username</h2>
-                    <p className={styles.bio}>Lover of tech, cats & coffee ☕</p>
+            <div className={styles.profile_info_container}>
+                <div className={styles.header}>
+                    <div className={styles.avatar}></div>
+                    <div>
+                        <h2 className={styles.username}>@username</h2>
+                        <p className={styles.bio}>Lover of tech, cats & coffee ☕</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className={styles.stats}>
-                <div>
-                    <p className={styles.stat_number}>34</p>
-                    <p className={styles.stat_label}>Posts</p>
-                </div>
-                <div>
-                    <p className={styles.stat_number}>128</p>
-                    <p className={styles.stat_label}>Followers</p>
-                </div>
-                <div>
-                    <p className={styles.stat_number}>97</p>
-                    <p className={styles.stat_label}>Following</p>
+                <div className={styles.stats}>
+                    <div>
+                        <p className={styles.stat_number}>34</p>
+                        <p className={styles.stat_label}>Posts</p>
+                    </div>
+                    <div>
+                        <p className={styles.stat_number}>128</p>
+                        <p className={styles.stat_label}>Followers</p>
+                    </div>
+                    <div>
+                        <p className={styles.stat_number}>97</p>
+                        <p className={styles.stat_label}>Following</p>
+                    </div>
                 </div>
             </div>
 
             <div className={styles.posts_section}>
                 <h3 className={styles.posts_title}>Recent Posts</h3>
                 <div className={styles.posts_list}>
-
+                    {[1, 2, 3].map((id) => (
+                        <Post key={id} user={"@user"} text={"This is my first post. Loving this platform"} time={"2"} />
+                    ))}
                 </div>
             </div>
         </div>
