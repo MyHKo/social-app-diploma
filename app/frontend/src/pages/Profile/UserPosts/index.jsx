53dcd7cc0186274b9c24e01a007dfc76b18d0b84
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-29 22:30:14 +0200

    Frontend: implenet user posts component

---
+++ app/frontend/src/pages/Profile/UserPosts/index.jsx
@@ -0,0 +1,22 @@
+import Post from '@components/Post/index.jsx'
+import PropTypes from 'prop-types'
+import styles from './userposts.module.scss'
+
+function UserPosts({ username }) {
+    return (
+        <div className={styles.posts_section}>
+            <h3 className={styles.posts_title}>Recent Posts</h3>
+            <div className={styles.posts_list}>
+                {[1, 2, 3].map((id) => (
+                    <Post key={id} user={"@user"} text={"This is my first post. Loving this platform"} time={"2"}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default UserPosts
+
+UserPosts.propTypes = {
+    username: PropTypes.string.isRequired,
+}
--- app/frontend/src/pages/Profile/index.jsx
+++ app/frontend/src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
-import Post from '@components/Post/index.jsx'
 import ProfileHeader from '@pages/Profile/ProfileHeader/index.jsx'
 import Stats from '@pages/Profile/Stats/index.jsx'
+import UserPosts from '@pages/Profile/UserPosts/index.jsx'
 import styles from './profile.module.scss'
 
 function Profile() {
@@ -12,14 +12,7 @@ function Profile() {
                 <Stats username={"username"}/>
             </div>
 
-            <div className={styles.posts_section}>
-                <h3 className={styles.posts_title}>Recent Posts</h3>
-                <div className={styles.posts_list}>
-                    {[1, 2, 3].map((id) => (
-                        <Post key={id} user={"@user"} text={"This is my first post. Loving this platform"} time={"2"} />
-                    ))}
-                </div>
-            </div>
+            <UserPosts username={"username"} />
         </div>
     )
 }
