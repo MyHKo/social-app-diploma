5954746e7ab03de52f4279d4bfcf4988921856a8
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-05-30 21:27:14 +0200

    Frontend: add like animation to the post component

--- app/frontend/src/components/Post/index.jsx
+++ app/frontend/src/components/Post/index.jsx
@@ -1,19 +1,28 @@
-import styles from './post.module.scss'
 import {Heart, MessageCircle} from 'lucide-react'
 import PropTypes from 'prop-types'
+import {useState} from 'react'
+import styles from './post.module.scss'
 
 function Post({ key, user, text, time }) {
+    const [isLiked, setIsLiked] = useState(false)
+
+    const toggleHeart = () => {
+        setIsLiked(!isLiked)
+    }
+
     return (
         <div className={styles.post} key={key}>
-            <div className={styles.postHeader}>
+            <div className={styles.post_header}>
                 <span className={styles.username}>@{user}</span>
                 <span className={styles.timestamp}>{time}h ago</span>
             </div>
-            <p className={styles.postContent}>
+            <p className={styles.post_content}>
                 {text}
             </p>
-            <div className={styles.postActions}>
-                <Heart className={styles.icon}/>
+            <div className={styles.post_actions}>
+                <div className={styles.heart_container} onClick={toggleHeart}>
+                    <Heart className={`${styles.heart_icon} ${isLiked ? styles.liked : ''}`}/>
+                </div>
                 <MessageCircle className={styles.icon}/>
             </div>
         </div>
