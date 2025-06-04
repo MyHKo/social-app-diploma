aeea2a8ac9cff48a631dee94ad409e542e8648b5
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-06-04 13:56:27 +0200

    Backend: implement jwt util file

--- app/backend/src/main/java/org/socialapp/Security/JWTUtil.java
+++ app/backend/src/main/java/org/socialapp/Security/JWTUtil.java
@@ -1,2 +1,62 @@
-package org.socialapp.Security.ConfFiles;public class JWTUtil {
+package org.socialapp.Security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private final PrivateKey privateKey;
+    private final PublicKey publicKey;
+
+    JWTUtil(PrivateKey privateKey, PublicKey publicKey) {
+        this.privateKey = privateKey;
+        this.publicKey = publicKey;
+    }
+
+    public String createToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + expiration))
+                .signWith(privateKey, Jwts.SIG.RS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(publicKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return true;
+        } catch (SecurityException e) {
+            System.out.println("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("JWT token is expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("JWT token is unsupported: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(publicKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
 }
