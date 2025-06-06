ff16fa6f3b448f1ddd6d0079d7914f36f142a8ae
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-06-06 20:12:25 +0200

    Frontend: add follow button to the profile page

--- app/frontend/src/pages/Profile/ProfileHeader/index.jsx
+++ app/frontend/src/pages/Profile/ProfileHeader/index.jsx
@@ -1,12 +1,17 @@
 import styles from './profileheader.module.scss'
 import PropTypes from "prop-types";
+import Button from "@components/UiKit/Button/Button.jsx";
 
 function ProfileHeader({ username, avatar }) {
     return (
         <div className={styles.header}>
             <div className={styles.avatar}>{avatar}</div>
             <div>
-                <h2 className={styles.username}>@{username}</h2>
+                <div className={styles.name_row}>
+                    <h2 className={styles.name}>John Doe</h2>
+                    <Button style={styles.follow_button} text="Follow"/>
+                </div>
+                <p className={styles.username}>@{username}</p>
                 <p className={styles.bio}>Lover of tech, cats & coffee ☕</p>
             </div>
         </div>
