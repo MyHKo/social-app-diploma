486dac8cd379aa1dd8260b6da33a6c5cec8f0035
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-06-11 10:41:43 +0200

    Backend: add a controller for post stats

--- app/backend/src/main/java/org/socialapp/controller/PostController.java
+++ app/backend/src/main/java/org/socialapp/controller/PostController.java
@@ -1,16 +1,22 @@
 package org.socialapp.controller;
 
+import org.socialapp.Service.CommentService;
+import org.socialapp.Service.LikeService;
 import org.socialapp.Service.PostService;
 import org.socialapp.Service.UserService;
 import org.socialapp.model.Entity.PostEntity;
 import org.socialapp.model.Entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
@@ -22,11 +28,29 @@ public class PostController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private LikeService likeService;
+
     @GetMapping("/{id}")
     public List<PostEntity> getPostsByUser_id(@PathVariable String id) {
         System.out.println(id);
         UserEntity user = userService.getUserByUsername(id);
         return postService.getPostsByUser(user);
+    }
+
+    @GetMapping("/stats-{id}")
+    public ResponseEntity<Map<String,Object>> getPostStats(@PathVariable String id) {
+        Map<String,Object> response = new HashMap<>();
+        Optional<PostEntity> post = postService.getPostById(Long.getLong(id));
+        int numberOfLikes = likeService.getLikesByPostId(post).size();
+        int numberOfComments = commentService.getCommentsByPost(post).size();
+        response.put("numberOfLikes", numberOfLikes);
+        response.put("numberOfComments", numberOfComments);
+
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/get-newest")
