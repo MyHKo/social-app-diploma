9d5d30eeb0e5301c1a13aeb23c99fc74877cd8b3
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-06-13 14:15:53 +0200

    Frontend: add loader icon to the post page

--- app/frontend/src/pages/PostPage/index.jsx
+++ app/frontend/src/pages/PostPage/index.jsx
@@ -4,6 +4,7 @@ import Textarea from '@components/UiKit/Textarea/Textarea.jsx'
 import Comment from '@components/Comment/index.jsx'
 import { useParams } from 'react-router'
 import { usePostStore } from '@stores/PostStore.js'
+import { LoaderCircle } from 'lucide-react'
 import calculateTimeDifference from '@utils/calculateTimeDifference.js'
 import {useEffect, useState} from 'react'
 import styles from './postPage.module.scss'
@@ -51,13 +52,16 @@ const PostPage = () => {
     return (
         <div className={styles.container}>
             <div className={styles.post_container}>
-                {post.id ? <Post key={post.id} postId={post.id} user={`${post.user_id.username}`} text={post.body}
-                       time={calculateTimeDifference(post.created_at)}
-                       title={post.title}
-                       number_of_likes={post.numberOfLikes}
-                       number_of_comments={post.numberOfComments}
-                />
-                    : "Loading"}
+                {post.id ?
+                        <Post key={post.id} postId={post.id} user={`${post.user_id.username}`} text={post.body}
+                           time={calculateTimeDifference(post.created_at)}
+                           title={post.title}
+                           number_of_likes={post.numberOfLikes}
+                           number_of_comments={post.numberOfComments}
+                        />
+                    :
+                        <LoaderCircle className={styles.post_loading}/>
+                }
             </div>
 
             <div className={styles.commentBox}>
@@ -66,14 +70,16 @@ const PostPage = () => {
             </div>
 
             <div className={styles.comment_container}>
-                {comments.length > 0 ?
-                    comments.map((comment) => (
-                    <Comment text={comment.body} time={calculateTimeDifference(comment.created_at)}
+                {comments.length > 0
+                    ?
+                        comments.map((comment) => (
+                            <Comment text={comment.body} time={calculateTimeDifference(comment.created_at)}
                              user={comment.user.username} key={comment.id} />
-                )) :
-                    <div className={styles.comment_placeholder}>
-                        <span>Oops.. so empty here</span>
-                    </div>
+                        ))
+                    :
+                        <div className={styles.comment_placeholder}>
+                            <span>Oops.. so empty here</span>
+                        </div>
                 }
             </div>
         </div>
