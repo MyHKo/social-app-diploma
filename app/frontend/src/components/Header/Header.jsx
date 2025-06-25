7ee12c96f0cb965d3110436c74f9748b377b844a
Mykola Horbenko <mykola.horbenko22@gmail.com>
2025-06-25 10:33:04 +0200

    Frontend: add login status to header

--- app/frontend/src/components/Header/Header.jsx
+++ app/frontend/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import Button from '@components/UiKit/Button/Button.jsx'
 import { useNavigate } from 'react-router'
 import routes from '@routes/path.js'
+import { useAuthStore } from '@stores/AuthStore.js'
 import styles from './header.module.scss'
 
 function Header() {
     const navigate = useNavigate()
+    const { isLoggedIn, setIsLoggedIn } = useAuthStore()
 
     return (
         <header className={styles.header}>
@@ -12,7 +14,10 @@ function Header() {
                 Social<span>Sphere</span>
             </h1>
             <div className={styles.loginButtonWrapper}>
-                <Button text={"Log In"} onClick={() => {navigate(routes.login)}}/>
+                {isLoggedIn
+                    ? <Button text={"Log In"} onClick={() => {navigate(routes.login)}}/>
+                    : <Button text={"Log Out"} style={styles.logOutButton}
+                              onClick={() => {console.log("user was logged out")}}/>}
             </div>
         </header>
     );
